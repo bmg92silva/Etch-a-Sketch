6cf78f5d07df0a47313cc2a3721e135597d4edb5
Bruno Silva <bruno92silva@outlook.pt>
2023-07-27 07:31:22 -0700

    Grid painting working

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
+/* ---------------------------- Change Grid with Slider -------------------------*/
+var slider = document.querySelector("#slider");
+var sliderValue = document.querySelector("#sliderValue");
+slider.addEventListener("input", function (event) {
+  let gridSize = event.target.value;
+  sliderValue.textContent = gridSize;
+  changeGridCSS(gridSize);
+  addChildGrid(gridSize);
+});
 
 /* ------ Change variable in CSS with number of columns and rows ------------- */
-let gridSize = 20;
-var root = document.querySelector(':root');
-var rootStyles = getComputedStyle(root);
-var deg= rootStyles.getPropertyValue('--gridSize');
-root.style.setProperty('--gridSize', gridSize);
-
-
-
-
-/* ---------------- Add childs to the grid container ------------------------ */
-const container = document.querySelector("#container");
-
-for (let i = 0; i < (gridSize*gridSize); i++) {
-  const gridItem = document.createElement("div");
-  gridItem.classList.add("gridItem");
-  gridItem.textContent = "";
-
-  container.appendChild(gridItem);
+function changeGridCSS(gridSize = 16) {
+  var root = document.querySelector(":root");
+  var rootStyles = getComputedStyle(root);
+  var deg = rootStyles.getPropertyValue("--gridSize");
+  root.style.setProperty("--gridSize", gridSize);
 }
 
-/* ------------------------------------------------------------------------------*/
+/* ---------------- Add childs to the grid container ------------------------ */
+function addChildGrid(gridSize = 16) {
+  const container = document.querySelector("#container");
+
+  removeAllChildNodes(container);
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("gridItem");
+    gridItem.setAttribute("id", i);
+    gridItem.textContent = "";
+
+    container.appendChild(gridItem);
+  }
+}
+
+/* -------------- Remove all child elements ---------------- */
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+
+  document.onmouseover = function (e) {
+    const gridTarget = e.target.id;
+    const classTarget = e.target.classList;
+
+    if (classTarget == "gridItem") {
+      // console.log(gridTarget);
+      e.target.style.backgroundColor = "black";
+    }
+  };
